2c720648451487c99d630271dc7981982e20d3aa
AYMANE El HOURRACH <spaskii@192.168.1.23>
2026-01-20 18:55:03 +0100

    Fix: Add link variant to Button component

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils"
 
 export interface ButtonProps
     extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    variant?: 'default' | 'outline' | 'ghost'
+    variant?: 'default' | 'outline' | 'ghost' | 'link'
     size?: 'default' | 'sm' | 'lg'
 }
 
@@ -17,6 +17,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
                         'bg-primary text-primary-foreground shadow hover:bg-primary/90': variant === 'default',
                         'border border-input bg-transparent shadow-sm hover:bg-secondary hover:text-secondary-foreground': variant === 'outline',
                         'hover:bg-accent hover:text-accent-foreground': variant === 'ghost',
+                        'text-primary underline-offset-4 hover:underline': variant === 'link',
                         'h-9 px-4 py-2': size === 'default',
                         'h-8 rounded-md px-3 text-xs': size === 'sm',
                         'h-10 rounded-md px-8': size === 'lg',
